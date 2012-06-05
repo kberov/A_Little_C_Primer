39a72c91ce8234cc74d6103c2bd84e59de29bc11
Krasimir Berov <k.berov@gmail.com>
2012-06-06 00:06:58 +0300

    Since there is only one statement in each clause the curly brackets aren't really necessary. This example would work just as well

--- ifelse.c
+++ ifelse.c
@@ -3,9 +3,11 @@
 #define MISSILE 1
 #define ROCKET 2
 void fire( int weapon );
+void fire_no_braces(int weapon);
 void main()
 {
   fire( ROCKET );
+  fire_no_braces( MISSILE );
 }
 
 void fire( int weapon )
@@ -17,4 +19,16 @@ void fire( int weapon )
   else {
     printf("Unknown weapon\n");
   }
+}
+
+/*
+ * Since there is only one statement in each clause the curly brackets aren't really necessary. This example would work just as well:
+ */
+ 
+void fire_no_braces(int weapon)
+{
+  if( weapon == MISSILE)
+    printf("Fired missile\n");
+  else 
+    printf("Unknown weapon\n");
 }
