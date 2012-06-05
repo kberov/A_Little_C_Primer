d9e3f752f7e5c726d0ab4f07a83172983c74abe1
Krasimir Berov <k.berov@gmail.com>
2012-06-06 00:26:15 +0300

    There is no "elseif" keyword, but "if" statements can be nested

---
+++ nestif.c
@@ -0,0 +1,24 @@
+/* nestif.c */
+#include <stdio.h>
+#define MISSILE 1
+#define LASER 2
+void fire( int weapon );
+void main()
+{
+  fire( LASER );
+}
+void fire( int weapon )
+{
+  if( weapon == MISSILE )
+  {
+    printf( "Fired missile!\n" );
+  }
+  else 
+  {
+    if( weapon == LASER )
+    {
+      printf( "Fired laser!\n" );
+    }
+  }
+}
+
